f3d0206baf816de7d96bfb009f4296bdfb68b94b
Shannan Kammann <sbkammann@gmail.com>
2018-06-18 15:52:26 -0700

    feat: Add makeGrid function

---
+++ js/app.js
@@ -0,0 +1,15 @@
+const parent = document.querySelector('#parent');
+
+function makeGrid(){
+  for (let i = 0; i < 4; i++){
+    const row = document.createElement('div');
+    parent.appendChild(row)
+    for (let j = 0; j < 4; j++){
+      const column = document.createElement('div');
+      column.setAttribute('class', 'cell');
+      row.appendChild(column)
+    }
+  }
+}
+
+makeGrid();
