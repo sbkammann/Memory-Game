3c180ade31fe0a873f1b5f2a23e27509d3a2b914
Shannan Kammann <sbkammann@gmail.com>
2018-06-18 18:24:41 -0700

    feat: Assign values to cardValue array

--- js/app.js
+++ js/app.js
@@ -1,5 +1,12 @@
 const parent = document.querySelector('#parent');
 
+// set up an array with values which will be assigned to cards to determine what the graphic will be and what card will pair with it
+let cardValue = [];
+for (let i=1; i<=8; i++){
+  cardValue[2*(i-1)]=[i];
+  cardValue[2*i-1]=[i];
+}
+
 // sets up 16 x 16 card grid
 function makeGrid(){
   for (let i = 0; i < 4; i++){
