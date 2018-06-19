11cd269e01105cd8a1793a9539548b940f3bb584
Shannan Kammann <sbkammann@gmail.com>
2018-06-19 14:46:08 -0700

    feat: Add star function
    Star function indicates the skill level. After too many moves stars are removed.

--- js/app.js
+++ js/app.js
@@ -5,7 +5,8 @@ let moveNum = 0;
 //winNum will be used to determine if the user won the game
 let winNum = null;
 let starNum = null;
-let plural = '!';
+//this is for the text that tells the user how many starts they had when they won. If they only have one star this gets set to '!'
+let plural = 's!';
 // set up an array with values which will be assigned to cards to determine what the graphic will be and what card will pair with it
 let cardValue = [];
 for (let i=1; i<=8; i++){
@@ -17,8 +18,8 @@ for (let i=1; i<=8; i++){
 if (document.URL.includes('index.html')){
 
   const parent = document.querySelector('#parent');
-  const movesCounter = document.querySelector('#movesCounter')
-
+  const movesCounter = document.querySelector('#movesCounter');
+  const starSpan = document.querySelector('#star');
   // sets up 16 x 16 card grid
   function makeGrid(){
     for (let i = 0; i < 4; i++){
@@ -103,7 +104,7 @@ if (document.URL.includes('index.html')){
     }
 
   movesCounter.textContent = moveNum.toString();
-  
+  star();
   }
 
   // reset the card variables
@@ -115,7 +116,23 @@ if (document.URL.includes('index.html')){
   function cookie(name, value) {
     document.cookie = name + "=" + value + ";";
   }
-
+  // sets the star skill rating
+  function star(){
+    let t ='***';
+    let starNum =3;
+    if (moveNum > 8 && moveNum < 16){
+      t ='**';
+      starNum =2;
+    }
+    if (moveNum >= 16){
+      t ='*';
+      plural ='!';
+      starNum =1;
+    }
+     starSpan.textContent = t;
+     sessionStorage.setItem('starNum', starNum.toString());
+     sessionStorage.setItem('plural', plural);
+  }
 
   makeGrid();
   randomNum();
@@ -129,10 +146,12 @@ else {
   const para = document.querySelector('p');
   const button = document.querySelector('button');
   const text1 = sessionStorage.getItem('moveNum');
-  let message = `With ${text1} Moves and ${starNum} Star${plural}`;
+  const text2 = sessionStorage.getItem('starNum');
+  const text3 = sessionStorage.getItem('plural');
+  let message = `With ${text1} Moves and ${text2} Star${text3}`;
   para.textContent = message;
   button.addEventListener('click', function load(){
-    sessionStorage.removeItem('moveNum');
+    sessionStorage.removeItem('moveNum', 'starNum', 'plural');
     window.location.href = "index.html";
   })
 }
